57f0d7387f2c33e5d41d6611e8407c35790c89b0
hasanabbasov <43010697+hasanabbasov@users.noreply.github.com>
2024-01-18 00:10:55 +0300

    Created spring boot project. Connected to DB

---
+++ src/main/java/com/example/FlightSearchAPI/FlightSearchApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.FlightSearchAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSearchApiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(FlightSearchApiApplication.class, args);
+	}
+
+}
